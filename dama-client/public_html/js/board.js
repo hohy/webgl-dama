455554b03ed6294514829e5e914d5d77f86edf72
hohy <jonnyxxx@gmail.com>
2013-03-31 15:23:14 +0200

    Added some improvements to the client.

--- dama-client/public_html/js/board.js
+++ dama-client/public_html/js/board.js
@@ -2,11 +2,11 @@
  * Board object
  */
 
-var Board = function() {
-    THREE.Object3D.call( this );
-    
     // number of fields on one side of the board
     var BOARD_SIZE = 8;
+
+var Board = function() {
+    THREE.Object3D.call( this );    
     
     // size of one board field
     var FIELD_SIZE = 1;
@@ -24,7 +24,7 @@ var Board = function() {
         for (var x = 0; x < BOARD_SIZE; x++) {
             // set a material to the field
             var fieldMaterial;
-            var fieldId = deskXYtoI(x,z);
+            var fieldId = boardXYtoI(x,z);
             if ((fieldId+z)%2==1) {
                     fieldMaterial = whiteFieldMaterial;
             } else {
@@ -36,16 +36,26 @@ var Board = function() {
             this.fields[fieldId].position.x = x + (FIELD_SIZE/2);
             this.fields[fieldId].position.z = z + (FIELD_SIZE/2);
             // set correct rotation
-            this.fields[fieldId].rotation.y = -1.57079633;
-            this.fields[fieldId].rotation.y = -1.57079633/2;
+            this.fields[fieldId].rotation.y = -Math.PI/2;
+            this.fields[fieldId].rotation.y = -Math.PI/4;
             // add the mesh to the scene
             this.add(this.fields[fieldId]);            
         }
-    }
+    }    
     
-    function deskXYtoI(x,y) {
-        return y * BOARD_SIZE + x;
-    }
 }
 
 Board.prototype = Object.create(THREE.Object3D.prototype);
+
+Board.prototype.getFieldPosition = function (x,y) {
+        var field = this.fields[boardXYtoI(x,y)];        
+        var vector = v(field.geometry.vertices.x,
+                       0,
+                       field.geometry.vertices.z); 
+        vector.applyMatrix4(field.matrixWorld);
+        return vector;
+    }
+    
+function boardXYtoI(x,y) {
+        return y * BOARD_SIZE + x;
+    }    
--- dama-client/public_html/js/dama.js
+++ dama-client/public_html/js/dama.js
@@ -1,71 +1,136 @@
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-			var renderer = new THREE.WebGLRenderer({antialias: true});
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
-			
-			var geometry = new THREE.CubeGeometry(0.51,0.51,0.51);
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.shadowMapEnabled = true;
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-			camera.position.z = 7;
-			camera.position.y = 3;		
-                        
-                        // axises
-                        var lineGeoX = new THREE.Geometry();
-                        var lineGeoY = new THREE.Geometry();
-                        var lineGeoZ = new THREE.Geometry();                        
-                        lineGeoX.vertices.push(v(-50, 0, 0), v(50, 0, 0));
-                        lineGeoY.vertices.push(v(0, -50, 0), v(0, 50, 0));
-                        lineGeoZ.vertices.push(v(0, 0, -50), v(0, 0, 50));
-                        var lineMatX = new THREE.LineBasicMaterial({color: 0xFF0000, lineWidth: 1});
-                        var lineMatY = new THREE.LineBasicMaterial({color: 0x00FF00, lineWidth: 1});
-                        var lineMatZ = new THREE.LineBasicMaterial({color: 0x0000FF, lineWidth: 1});
-                        var lineX = new THREE.Line(lineGeoX, lineMatX);
-                        var lineY = new THREE.Line(lineGeoY, lineMatY);
-                        var lineZ = new THREE.Line(lineGeoZ, lineMatZ);
-                        lineX.type = THREE.Lines;
-                        lineY.type = THREE.Lines;
-                        lineZ.type = THREE.Lines;                        
-                        scene.add(lineX);
-                        scene.add(lineY);
-                        scene.add(lineZ);
-                        			                        
-                        // Figure test
-                        var testFigure = new Figure(FIGURE_COLOR.BLACK);
-                        scene.add(testFigure);
-                        
-                        // Board test
-                        var board = new Board();                        
-                        scene.add(board);
-                        board.position.x = -4;
-                        board.position.z = -4;
-			
-			var time = 0;
-			function render() {
-				requestAnimationFrame(render);
-			    cube.rotation.x += 0.1;
-				cube.rotation.y += 0.1;	
-				time += 0.01;
-				camera.position.x = 6 * Math.cos( time );         
-				camera.position.z = 6 * Math.sin( time );
-				camera.lookAt( new THREE.Vector3(0, 0, 0));				
-				renderer.render(scene, camera);
-			}
-						
-			render();
-                        
-                        // various functions
-                        
-                        /**
-                         * Create new vertex
-                         * @param {type} x
-                         * @param {type} y
-                         * @param {type} z
-                         * @returns {@exp;THREE@call;Vertex}
-                         */
-                        function v(x,y,z){ 
-                            return new THREE.Vertex(new THREE.Vector3(x,y,z)); 
-                        }
+var geometry = new THREE.CubeGeometry(0.51,0.51,0.51);
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+camera.position.z = 7;
+camera.position.y = 3;		                       
+
+showAxises();                        
+
+// add game board to scene
+var board = new Board();                        
+scene.add(board);
+board.position.x = -4;
+board.position.z = -4;
+board.updateMatrixWorld();
+
+// add figures to the scene
+var figures = new Array();
+var figureId = 0;
+// black ones
+for(var y=0; y < 3; y++) {    
+    for(var x=y%2; x < 8; x=x+2) {
+        figures[figureId] = new Figure(FIGURE_COLOR.BLACK);
+        scene.add(figures[figureId]);
+        figures[figureId++].position = board.getFieldPosition(x,y);    
+    }                            
+}
+// white ones
+for(var y=0; y < 3; y++) {
+    for(var x=(y+1)%2; x < 8; x=x+2) {
+        figures[figureId] = new Figure(FIGURE_COLOR.WHITE);
+        scene.add(figures[figureId]);
+        figures[figureId++].position = board.getFieldPosition(x,7-y);    
+    }                            
+}
+
+// Board field possition test
+var fldLineGeo = new THREE.Geometry();
+fldLineGeo.vertices.push(v(0,0.5,0),v(0,-0.5,0));
+var fldLineMatHilited  = new THREE.LineBasicMaterial({color: 0xFF0000, lineWidth: 1});
+var fldLineMat = new THREE.LineBasicMaterial({color: 0x000000, lineWidth: 1});
+var fldLines = new Array();
+fldLines[0] = new THREE.Line(fldLineGeo,fldLineMatHilited);
+fldLines[0].type = THREE.Lines;                        
+scene.add(fldLines[0]);
+fldLines[0].position = board.getFieldPosition(0,0);
+fldLines[1] = new THREE.Line(fldLineGeo,fldLineMat);
+fldLines[1].type = THREE.Lines;                        
+scene.add(fldLines[1]);
+fldLines[1].position = board.getFieldPosition(7,7);
+
+// Lights
+scene.add( new THREE.AmbientLight( 0x444444 ) );
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+directionalLight.position.set( 1, 1, 1 ).normalize();
+scene.add( directionalLight );
+
+var pointLight = new THREE.PointLight( 0xffffff, 2, 800 );
+scene.add( pointLight );
+
+var particleLight = new THREE.Mesh( new THREE.SphereGeometry( 4, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
+particleLight.material.color = pointLight.color;
+pointLight.position = particleLight.position;
+                                
+document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+var projector = new THREE.Projector();
+
+var time = 0;
+
+function render() {
+        requestAnimationFrame(render);
+        time += 0.005;
+        camera.position.x = 6 * Math.cos( time );         
+        camera.position.z = 6 * Math.sin( time );
+        camera.lookAt( new THREE.Vector3(0, 0, 0));
+        particleLight.position.x = Math.sin( time * 7 ) * 300;
+	particleLight.position.y = Math.cos( time * 5 ) * 400;
+	particleLight.position.z = Math.cos( time * 3 ) * 300;        
+        renderer.render(scene, camera);
+}
+
+render();
+
+// various functions
+
+/**
+ * Create new vertex
+ * @param {type} x
+ * @param {type} y
+ * @param {type} z
+ * @returns {@exp;THREE@call;Vertex}
+ */
+function v(x,y,z){ 
+    return new THREE.Vertex(new THREE.Vector3(x,y,z)); 
+}
+
+function showAxises() {
+    var lineGeoX = new THREE.Geometry();
+    var lineGeoY = new THREE.Geometry();
+    var lineGeoZ = new THREE.Geometry();                        
+    lineGeoX.vertices.push(v(-50, 0, 0), v(50, 0, 0));
+    lineGeoY.vertices.push(v(0, -50, 0), v(0, 50, 0));
+    lineGeoZ.vertices.push(v(0, 0, -50), v(0, 0, 50));
+    var lineMatX = new THREE.LineBasicMaterial({color: 0xFF0000, lineWidth: 1});
+    var lineMatY = new THREE.LineBasicMaterial({color: 0x00FF00, lineWidth: 1});
+    var lineMatZ = new THREE.LineBasicMaterial({color: 0x0000FF, lineWidth: 1});
+    var lineX = new THREE.Line(lineGeoX, lineMatX);
+    var lineY = new THREE.Line(lineGeoY, lineMatY);
+    var lineZ = new THREE.Line(lineGeoZ, lineMatZ);
+    lineX.type = THREE.Lines;
+    lineY.type = THREE.Lines;
+    lineZ.type = THREE.Lines;                        
+    scene.add(lineX);
+    scene.add(lineY);
+    scene.add(lineZ);                            
+}
+
+function onDocumentMouseDown( event ) {
+    event.preventDefault();
+    var vector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1,
+        - ( event.clientY / window.innerHeight ) * 2 + 1, 0.5 );
+    projector.unprojectVector( vector, camera );
+    var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+    var intersects = raycaster.intersectObjects( figures );
+    if ( intersects.length > 0 ) {
+        intersects[0].object.material.color.setHex( Math.random() * 0xffffff );
+        console.log("Click on the figure.");
+    }
+}
